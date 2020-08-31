96c633df4a97c0f861c658e9cb0b1903ea626ab7
Marco Prado <antonio.123147@hotmail.com>
2020-08-30 23:53:48 -0500

    first commit

---
+++ data.py
@@ -0,0 +1,67 @@
+ARAD = 0
+BUCHAREST = 1
+CRAIOVA = 2
+DOBRETA = 3
+EFORIE = 4
+FAGARAS = 5
+GIURGIU = 6
+HIRSOVA = 7
+IASI = 8
+LUGOJ = 9
+MEHADIA = 10
+NEAMT = 11
+ORADEA = 12
+PITESTI = 13
+RIMNICUVILCEA = 14
+SIBIU = 15
+TIMISOARA = 16
+URZICENI = 17
+VASLUI = 18
+ZERIND = 19
+
+distanceToBucharest = [366, 0, 160, 242, 161, 178, 77, 151, 226, 244,
+                       241, 234, 380, 98, 193, 253, 329, 80, 199, 374]
+
+connects = [
+    [(ZERIND, 75), (SIBIU, 140), (TIMISOARA, 118)],
+    [(PITESTI, 101), (FAGARAS, 211), (GIURGIU, 90), (URZICENI, 85)],
+    [(DOBRETA, 120), (PITESTI, 138), (RIMNICUVILCEA, 146)],
+    [(MEHADIA, 75), (CRAIOVA, 120)],
+    [(HIRSOVA, 86)],
+    [(SIBIU, 99), (BUCHAREST, 211)],
+    [(BUCHAREST, 90)],
+    [(URZICENI, 98), (EFORIE, 86)],
+    [(NEAMT, 87), (VASLUI, 92)],
+    [(TIMISOARA, 111), (MEHADIA, 70)],
+    [(LUGOJ, 70), (DOBRETA, 75)],
+    [(IASI, 87)],
+    [(ZERIND, 71), (SIBIU, 151)],
+    [(RIMNICUVILCEA, 97), (CRAIOVA, 138), (BUCHAREST, 101)],
+    [(SIBIU, 80), (CRAIOVA, 146), (PITESTI, 97)],
+    [(ARAD, 140), (ORADEA, 151), (FAGARAS, 99), (RIMNICUVILCEA, 80)],
+    [(ARAD, 118), (LUGOJ, 111)],
+    [(BUCHAREST, 85), (VASLUI, 142), (HIRSOVA, 98)],
+    [(IASI, 92), (URZICENI, 142)],
+    [(ORADEA, 71), (ARAD, 75)]
+]
+
+names = ["ARAD",
+         "BUCHAREST",
+         "CRAIOVA",
+         "DOBRETA",
+         "EFORIE",
+         "FAGARAS",
+         "GIURGIU",
+         "HIRSOVA",
+         "IASI",
+         "LUGOJ",
+         "MEHADIA",
+         "NEAMT",
+         "ORADEA",
+         "PITESTI",
+         "RIMNICUVILCEA",
+         "SIBIU",
+         "TIMISOARA",
+         "URZICENI",
+         "VASLUI",
+         "ZERIND"]
